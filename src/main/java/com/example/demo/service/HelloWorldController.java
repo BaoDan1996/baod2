fef7292f37ce05cb576b2d6f219b60a521bad7eb
baodan <1181857068@qq.com>
2023-04-25 22:11:17 +0800

    test4

--- src/main/java/com/example/demo/service/HelloWorldController.java
+++ src/main/java/com/example/demo/service/HelloWorldController.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-
     @RequestMapping("/")
     public String sayHello(){
         return "Hello,World!";
     }
+
 }
