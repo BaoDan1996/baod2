aeaf48e612536e4190f134f17d4fe6d9bfcd6bb3
baodan <1181857068@qq.com>
2023-04-26 22:00:06 +0800

    test5

---
+++ src/main/java/com/example/demo/bean/User.java
@@ -0,0 +1,36 @@
+package com.example.demo.bean;
+
+/**
+ * @author: baodan
+ * @date: 2023/4/26
+ * @version: 1.1.0
+ **/
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
