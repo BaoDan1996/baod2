b5c0a10c24c5a9c881983b9d0dd74f77c768b9e9
baodan <1181857068@qq.com>
2025-03-23 15:57:23 +0800

    version 1.0.0

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+/**
+ * @author: baodan
+ * @date: 2025/3/23
+ * @version: 1.0.0
+ **/
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
